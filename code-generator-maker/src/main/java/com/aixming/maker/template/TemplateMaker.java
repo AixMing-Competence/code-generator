2a8158971f17c830dba122506b4ea63bf1201827
AixMing <970338371@qq.com>
2024-09-03 20:23:16 +0800

    追加模板文件的修改和配置文件的生成（单文件）

--- code-generator-maker/src/main/java/com/aixming/maker/template/TemplateMaker.java
+++ code-generator-maker/src/main/java/com/aixming/maker/template/TemplateMaker.java
@@ -12,6 +12,7 @@ import java.io.File;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * 模板制作工具
@@ -21,79 +22,155 @@ import java.util.List;
  */
 public class TemplateMaker {
 
-    public static void main(String[] args) {
-        // 源项目路径
-        String projectPath = System.getProperty("user.dir");
-        String originProjectPath = new File(projectPath).getParent() + File.separator + "acm-template-pro-demo";
-        String tempDirPath = projectPath + File.separator + ".temp";
-
-        long snowflakeNextId = IdUtil.getSnowflakeNextId();
-        String templatePath = tempDirPath + File.separator + snowflakeNextId;
-        if (!FileUtil.exist(templatePath)) {
-            FileUtil.mkdir(templatePath);
+    private static long makeTemplate(String originProjectPath, String inputFilePath, Meta newMeta, Meta.ModelConfig.ModelInfo modelInfo, String searchStr, Long id) {
+        if (id == null) {
+            id = IdUtil.getSnowflakeNextId();
         }
 
-        // 复制目录，创建工作空间
-        FileUtil.copy(originProjectPath, templatePath, true);
+        // 源项目路径
+        String projectPath = System.getProperty("user.dir");
+        String tempDirPath = projectPath + File.separator + ".temp";
+
+        String templatePath = tempDirPath + File.separator + id;
+        if (!FileUtil.exist(templatePath)) {
+            FileUtil.mkdir(templatePath);
+            // 复制目录，创建工作空间
+            FileUtil.copy(originProjectPath, templatePath, true);
+        }
 
         // 一、输入信息
         // 1、项目基本信息
-        String name = "acm-template-pro-generator";
-        String description = "算法模板生成器";
+        String name = newMeta.getName();
+        String description = newMeta.getDescription();
 
         // 2、文件输入信息
+        // 要挖坑的项目根目录
         String sourceRootPath = templatePath + File.separator + FileUtil.getLastPathEle(Paths.get(originProjectPath));
         sourceRootPath = sourceRootPath.replaceAll("\\\\", "/");
-        String inputFilePath = "src/main/java/com/aixming/MainTemplate.java";
+        // 要挖坑的文件
         String outputFilePath = inputFilePath + ".ftl";
 
         // 3、输入模型参数信息
-        Meta.ModelConfig.ModelInfo modelInfo = new Meta.ModelConfig.ModelInfo();
-        modelInfo.setFieldName("outputText");
-        modelInfo.setType("String");
-        modelInfo.setDefaultValue("sum = ");
 
         // 二、替换字符串，生成模板文件
         String inputFileAbsolutePath = sourceRootPath + File.separator + inputFilePath;
-        String fileContent = FileUtil.readUtf8String(inputFileAbsolutePath);
+        String outputFileAbsolutePath = sourceRootPath + File.separator + outputFilePath;
+        String fileContent;
+
+        // 如果已有模板文件，表示已经挖过坑，则在模板文件的基础上再挖坑
+        if (FileUtil.exist(outputFileAbsolutePath)) {
+            fileContent = FileUtil.readUtf8String(outputFileAbsolutePath);
+        } else {
+            fileContent = FileUtil.readUtf8String(inputFileAbsolutePath);
+        }
+
         // 替换源文件内容
         String replacement = String.format("${%s}", modelInfo.getFieldName());
-        String newFileContent = StrUtil.replace(fileContent, "sum", replacement);
+        String newFileContent = StrUtil.replace(fileContent, searchStr, replacement);
         // 输出模板文件
-        String outputFileAbsolutePath = sourceRootPath + File.separator + outputFilePath;
         FileUtil.writeUtf8String(newFileContent, outputFileAbsolutePath);
 
-        // 三、生成配置文件（meta.json）
-        String metaOutputPath = sourceRootPath + File.separator + "meta.json";
-
-        Meta meta = new Meta();
-        meta.setName(name);
-        meta.setDescription(description);
-
-        Meta.FileConfig fileConfig = new Meta.FileConfig();
-        meta.setFileConfig(fileConfig);
-
-        fileConfig.setSourceRootPath(sourceRootPath);
-        List<Meta.FileConfig.FileInfo> fileInfoList = new ArrayList<>();
-        fileConfig.setFiles(fileInfoList);
-
+        // 文件配置信息
         Meta.FileConfig.FileInfo fileInfo = new Meta.FileConfig.FileInfo();
-        fileInfoList.add(fileInfo);
         fileInfo.setInputPath(inputFilePath);
         fileInfo.setOutputPath(outputFilePath);
         fileInfo.setType(FileTypeEnum.FILE.getValue());
         fileInfo.setGenerateType(FileGenerateTypeEnum.DYNAMIC.getValue());
 
-        Meta.ModelConfig modelConfig = new Meta.ModelConfig();
-        meta.setModelConfig(modelConfig);
-        List<Meta.ModelConfig.ModelInfo> modelInfoList = new ArrayList<>();
-        modelConfig.setModels(modelInfoList);
-        modelInfoList.add(modelInfo);
-        System.out.println();
+        // 三、生成配置文件（meta.json）
+        String metaOutputPath = sourceRootPath + File.separator + "meta.json";
 
-        String jsonStr = JSONUtil.toJsonPrettyStr(meta);
-        FileUtil.writeUtf8String(jsonStr, metaOutputPath);
+        if (FileUtil.exist(metaOutputPath)) {
+            // 1、追加配置参数
+            Meta oldMeta = JSONUtil.toBean(FileUtil.readUtf8String(metaOutputPath), Meta.class);
 
+            List<Meta.FileConfig.FileInfo> fileInfoList = oldMeta.getFileConfig().getFiles();
+            fileInfoList.add(fileInfo);
+
+            List<Meta.ModelConfig.ModelInfo> modelInfoList = oldMeta.getModelConfig().getModels();
+            modelInfoList.add(modelInfo);
+            
+            oldMeta.getFileConfig().setFiles(distinctFiles(fileInfoList));
+            oldMeta.getModelConfig().setModels(distinctModels(modelInfoList));
+
+            // 2、输出元信息文件
+            FileUtil.writeUtf8String(JSONUtil.toJsonPrettyStr(oldMeta), metaOutputPath);
+
+        } else {
+            // 1、构造配置参数对象
+
+            Meta.FileConfig fileConfig = new Meta.FileConfig();
+            newMeta.setFileConfig(fileConfig);
+
+            fileConfig.setSourceRootPath(sourceRootPath);
+            List<Meta.FileConfig.FileInfo> fileInfoList = new ArrayList<>();
+            fileConfig.setFiles(fileInfoList);
+            fileInfoList.add(fileInfo);
+
+            Meta.ModelConfig modelConfig = new Meta.ModelConfig();
+            newMeta.setModelConfig(modelConfig);
+            List<Meta.ModelConfig.ModelInfo> modelInfoList = new ArrayList<>();
+            modelConfig.setModels(modelInfoList);
+            modelInfoList.add(modelInfo);
+
+            // 2、输出元信息文件
+            FileUtil.writeUtf8String(JSONUtil.toJsonPrettyStr(newMeta), metaOutputPath);
+        }
+
+        return id;
     }
+
+    /**
+     * 文件去重
+     *
+     * @param fileInfoList
+     * @return
+     */
+    private static List<Meta.FileConfig.FileInfo> distinctFiles(List<Meta.FileConfig.FileInfo> fileInfoList) {
+        ArrayList<Meta.FileConfig.FileInfo> newFileInfoList = new ArrayList<>(fileInfoList.stream()
+                .collect(Collectors.toMap(Meta.FileConfig.FileInfo::getInputPath, item -> item, (e, r) -> r))
+                .values());
+        return newFileInfoList;
+    }
+
+    /**
+     * 模型去重
+     *
+     * @param modelInfoList
+     * @return
+     */
+    private static List<Meta.ModelConfig.ModelInfo> distinctModels(List<Meta.ModelConfig.ModelInfo> modelInfoList) {
+        ArrayList<Meta.ModelConfig.ModelInfo> newModelInfoList = new ArrayList<>(modelInfoList.stream()
+                .collect(Collectors.toMap(Meta.ModelConfig.ModelInfo::getFieldName, item -> item, (e, r) -> r))
+                .values());
+        return newModelInfoList;
+    }
+
+    public static void main(String[] args) {
+        String projectPath = System.getProperty("user.dir");
+        String originProjectPath = new File(projectPath).getParent() + File.separator + "acm-template-pro-demo";
+        String name = "acm-template-pro-generator";
+        String description = "算法模板生成器";
+        Meta meta = new Meta();
+        meta.setName(name);
+        meta.setDescription(description);
+        String inputFilePath = "src/main/java/com/aixming/MainTemplate.java";
+        
+        // 第一次挖坑
+        // Meta.ModelConfig.ModelInfo modelInfo = new Meta.ModelConfig.ModelInfo();
+        // modelInfo.setFieldName("outputText");
+        // modelInfo.setType("String");
+        // modelInfo.setDefaultValue("sum");
+        
+        // 第二次挖坑
+        Meta.ModelConfig.ModelInfo modelInfo = new Meta.ModelConfig.ModelInfo();
+        modelInfo.setFieldName("className");
+        modelInfo.setType("String");
+        
+        // String searchStr = "sum";
+        String searchStr = "MainTemplate";
+        makeTemplate(originProjectPath, inputFilePath, meta, modelInfo, searchStr, 1L);
+    }
+
 
 }
