e6e4b8a54d3a0f041210b6e98bc3ec3ff2e61f34
AixMing <970338371@qq.com>
2024-09-03 13:49:32 +0800

    根据源文件生成模板（字符串替换），工作空间隔离，不污染源文件

--- code-generator-maker/src/main/java/com/aixming/maker/template/TemplateMaker.java
+++ code-generator-maker/src/main/java/com/aixming/maker/template/TemplateMaker.java
@@ -1,6 +1,7 @@
 package com.aixming.maker.template;
 
 import cn.hutool.core.io.FileUtil;
+import cn.hutool.core.util.IdUtil;
 import cn.hutool.core.util.StrUtil;
 import cn.hutool.json.JSONUtil;
 import com.aixming.maker.enums.FileGenerateTypeEnum;
@@ -8,26 +9,41 @@ import com.aixming.maker.enums.FileTypeEnum;
 import com.aixming.maker.meta.Meta;
 
 import java.io.File;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
 /**
  * 模板制作工具
+ *
  * @author AixMing
  * @since 2024-09-02 20:26:53
  */
 public class TemplateMaker {
 
     public static void main(String[] args) {
+        // 源项目路径
+        String projectPath = System.getProperty("user.dir");
+        String originProjectPath = new File(projectPath).getParent() + File.separator + "acm-template-pro-demo";
+        String tempDirPath = projectPath + File.separator + ".temp";
+
+        long snowflakeNextId = IdUtil.getSnowflakeNextId();
+        String templatePath = tempDirPath + File.separator + snowflakeNextId;
+        if (!FileUtil.exist(templatePath)) {
+            FileUtil.mkdir(templatePath);
+        }
+
+        // 复制目录，创建工作空间
+        FileUtil.copy(originProjectPath, templatePath, true);
+
         // 一、输入信息
         // 1、项目基本信息
         String name = "acm-template-pro-generator";
         String description = "算法模板生成器";
 
         // 2、文件输入信息
-        String projectPath = System.getProperty("user.dir");
-        String sourceRootPath = new File(projectPath).getParent() + File.separator + "acm-template-pro-demo";
-        sourceRootPath = sourceRootPath.replaceAll("\\\\","/");
+        String sourceRootPath = templatePath + File.separator + FileUtil.getLastPathEle(Paths.get(originProjectPath));
+        sourceRootPath = sourceRootPath.replaceAll("\\\\", "/");
         String inputFilePath = "src/main/java/com/aixming/MainTemplate.java";
         String outputFilePath = inputFilePath + ".ftl";
 
@@ -36,17 +52,17 @@ public class TemplateMaker {
         modelInfo.setFieldName("outputText");
         modelInfo.setType("String");
         modelInfo.setDefaultValue("sum = ");
-        
+
         // 二、替换字符串，生成模板文件
         String inputFileAbsolutePath = sourceRootPath + File.separator + inputFilePath;
         String fileContent = FileUtil.readUtf8String(inputFileAbsolutePath);
         // 替换源文件内容
-        String replacement = String.format("${%s}",modelInfo.getFieldName());
+        String replacement = String.format("${%s}", modelInfo.getFieldName());
         String newFileContent = StrUtil.replace(fileContent, "sum", replacement);
         // 输出模板文件
         String outputFileAbsolutePath = sourceRootPath + File.separator + outputFilePath;
-        FileUtil.writeUtf8String(newFileContent,outputFileAbsolutePath);
-        
+        FileUtil.writeUtf8String(newFileContent, outputFileAbsolutePath);
+
         // 三、生成配置文件（meta.json）
         String metaOutputPath = sourceRootPath + File.separator + "meta.json";
 
@@ -56,11 +72,11 @@ public class TemplateMaker {
 
         Meta.FileConfig fileConfig = new Meta.FileConfig();
         meta.setFileConfig(fileConfig);
-        
+
         fileConfig.setSourceRootPath(sourceRootPath);
         List<Meta.FileConfig.FileInfo> fileInfoList = new ArrayList<>();
         fileConfig.setFiles(fileInfoList);
-        
+
         Meta.FileConfig.FileInfo fileInfo = new Meta.FileConfig.FileInfo();
         fileInfoList.add(fileInfo);
         fileInfo.setInputPath(inputFilePath);
@@ -76,8 +92,8 @@ public class TemplateMaker {
         System.out.println();
 
         String jsonStr = JSONUtil.toJsonPrettyStr(meta);
-        FileUtil.writeUtf8String(jsonStr,metaOutputPath);
+        FileUtil.writeUtf8String(jsonStr, metaOutputPath);
 
     }
-    
+
 }
